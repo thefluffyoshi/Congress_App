5386737930d8a8a67feb34a896325c61591e6edd
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-10-17 14:48:48 -0700

    Trig button changes text to show trig, set opposites on the right

--- app/src/main/java/com/example/congressapp/Math.java
+++ app/src/main/java/com/example/congressapp/Math.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Math extends AppCompatActivity {
 
@@ -30,6 +31,8 @@ public class Math extends AppCompatActivity {
     {
         ImageView chart = (ImageView)findViewById(R.id.flowChartIV);
         chart.setVisibility(View.INVISIBLE);
+        TextView trig = (TextView)findViewById(R.id.MathTV);
+        trig.setText("Opp = Opposite   Hyp = Hypotenuse   Adj = Adjacent \n sinθ = opp/hyp   cscθ = hyp/opp \n cosθ = adj/hyp   secθ = hyp/adj \n tanθ = opp/adj   cotθ = adj/opp");
     }
 
     //coterminal angles
