ad74cfbd9b02742b65e6adac66860249813e58e5
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-10-17 14:36:54 -0700

    added tip 3 onClick

--- app/src/main/java/com/example/congressapp/ASTipsNTricks.java
+++ app/src/main/java/com/example/congressapp/ASTipsNTricks.java
@@ -32,5 +32,11 @@ public class ASTipsNTricks extends AppCompatActivity {
         tip2.setText("If you notice the same code being copied and pasted into multiple different methods within a class, make a private class using private void. Then copy the code into the private class and call it when it's needed.");
     }
 
+    public void onClickTip3(View view)
+    {
+        TextView tip3 = (TextView) findViewById(R.id.TipsTV);
+        tip3.setText("You can set an image to be invisible through the .xml file by putting an Image View in the design, then in the .xml code put in android:visibility=\"invisible\". Then in the .java file make an OnClick method put ImageView (var name) = (ImageView)findViewById(R.id.(ID)); Underneath that, you would use the var name to set visibility by (var name).setVisibility(View.VISIBLE);");
+    }
+
     //Add information on
 }
