7f745db49906f0964a91aa41036d5a535ef5c7e4
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-10-18 00:29:58 -0700

    added home screen onClick in Japanese Screen

--- app/src/main/java/com/example/congressapp/Japanese.java
+++ app/src/main/java/com/example/congressapp/Japanese.java
@@ -2,7 +2,9 @@ package com.example.congressapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Japanese extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Japanese extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_japanese);
+    }
+
+    public void onClickbth(View view)
+    {
+        startActivity(new Intent(Japanese.this, MainActivity.class));
     }
 }
