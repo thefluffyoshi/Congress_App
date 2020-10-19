723a9d97396e271f29658be5f36637b61b1130b4
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-10-18 18:51:00 -0700

    added trig identities, linked onClick trig identities added coterminal, added limits

--- app/src/main/java/com/example/congressapp/Math.java
+++ app/src/main/java/com/example/congressapp/Math.java
@@ -25,7 +25,10 @@ public class Math extends AppCompatActivity {
 
     public void onClickFFC(View view)
     {
-        updateIVFFC();
+        ImageView chart = (ImageView) findViewById(R.id.flowChartIV);
+        chart.setVisibility(View.VISIBLE);
+        TextView mathTV = (TextView) findViewById(R.id.MathTV);
+        mathTV.setText("");
     }
 
     public void onClickTrig(View view)
@@ -35,18 +38,26 @@ public class Math extends AppCompatActivity {
         trig.setText("Opp = Opposite   Hyp = Hypotenuse   Adj = Adjacent \n sinθ = opp/hyp   cscθ = hyp/opp \n cosθ = adj/hyp   secθ = hyp/adj \n tanθ = opp/adj   cotθ = adj/opp");
     }
 
+    public void onClickTrigIdentities(View view)
+    {
+        updateIVFFC();
+        TextView trigIdentities = (TextView) findViewById(R.id.MathTV);
+        trigIdentities.setText("sin²θ + cos²θ = 1 \n 1 + tan²θ = sec²θ \n 1 + cot²θ = csc²θ \nsin2θ = 2sinθcosθ \ncos2θ = cos²θ - sin²θ");
+    }
+
     //coterminal angles
     public void onClickCoterminalAngles()
     {
         updateIVFFC();
         TextView CA = (TextView) findViewById(R.id.MathTV);
+        CA.setText("To find a coterminal angle, you add 360 twice to the given angle. Then subtract 360 from the given angle.");
     }
 
     public void onClickLimits(View view)
     {
         updateIVFFC();
         TextView limits = (TextView) findViewById(R.id.MathTV);
-        limits.setText("");
+        limits.setText("lim x→(value) f(x). To find a limit, you input the value that x is approaching. Without graphing, you can find what f(x) is approaching from the left and right of the x value. If the denominator is 0 or the value on top is being divided by 0 then you would factor to cancel out terms. Then reinput the x value into the factored out form.");
     }
 
     private void updateIVFFC()
