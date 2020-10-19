b49345a09913eb807af150b42e1346fd3914042b
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-10-18 23:05:34 -0700

    Added body language

--- app/src/main/java/com/example/congressapp/Psychology.java
+++ app/src/main/java/com/example/congressapp/Psychology.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Psychology extends AppCompatActivity {
 
@@ -17,5 +18,11 @@ public class Psychology extends AppCompatActivity {
     public void onClickBckHome(View view)
     {
         startActivity(new Intent(Psychology.this, MainActivity.class));
+    }
+
+    public void onClickBodyLang(View view)
+    {
+        TextView bodylang = (TextView) findViewById(R.id.psychTV);
+        bodylang.setText("When a person retells a story, they will recount the details of a story instead of only the main thing that happened. Generally, liars are nervous and may look around to try and find an escape or to avoid answering. Usually people will fidget when lying because they are nervous about getting caught. Focus on emotions or lack of emotions and incongruent movements/facial expressions.");
     }
 }
