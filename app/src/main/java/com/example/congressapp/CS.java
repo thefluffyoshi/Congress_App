837cca4b30510a6f9c08a2542fa4545616702ce2
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-10-18 22:51:22 -0700

    added onClickforloop, added onClickArray

--- app/src/main/java/com/example/congressapp/CS.java
+++ app/src/main/java/com/example/congressapp/CS.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class CS extends AppCompatActivity {
 
@@ -17,6 +18,31 @@ public class CS extends AppCompatActivity {
     public void onClickBTH(View view)
     {
         startActivity(new Intent(CS.this, MainActivity.class));
+    }
+
+    public void onClickForLoop(View view)
+    {
+        TextView forloop = (TextView) findViewById(R.id.csTV);
+        forloop.setText("A for loop consists of:" +
+                        "\nfor (int i = 0; i < 5; i++)" +
+                        "\n{" +
+                        "\n\t(the brain method)" +
+                        "\n}");
+    }
+
+    public void onClickArray(View view)
+    {
+        TextView array = (TextView) findViewById(R.id.csTV);
+        array.setText("You can use a for loop for an array. You can change the values at the end of each intArray[]. You would use a for loop to do calculations without copy and pasting code over and over, to print out all of the values or to change values." +
+                      "\nint [] intArray = new int[100];" +
+                      "\nintArray[0] = 110;" +
+                      "\nintArray[1] = 100" +
+                      "\nintArray[2] = 99" +
+                      "\nintArray[3] = 89" +
+                      "\n for (int i = 100; i > 0; i--)" +
+                      "\n{" +
+                      "\n  System.out.println(intArray[i]);" +
+                      "\n}");
     }
 
 
