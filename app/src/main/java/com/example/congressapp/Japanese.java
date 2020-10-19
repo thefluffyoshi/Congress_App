330195e55c3dfd97373fc8a045823be684084906
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-10-18 18:49:40 -0700

    added hiragana characters, put them symmetrical

--- app/src/main/java/com/example/congressapp/Japanese.java
+++ app/src/main/java/com/example/congressapp/Japanese.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Japanese extends AppCompatActivity {
 
@@ -17,5 +18,19 @@ public class Japanese extends AppCompatActivity {
     public void onClickbth(View view)
     {
         startActivity(new Intent(Japanese.this, MainActivity.class));
+    }
+
+    public void onClickHiragana(View view)
+    {
+        TextView hiragana = (TextView) findViewById(R.id.JapTV);
+        hiragana.setText(
+                        "  a = あ         ki = き         so = そ         ni = に" +
+                        "\n   i = い        ku = く          ta = た       nu = ぬ" +
+                        "\n  u = う        ke = け         chi = ち       ne = ね" +
+                        "\n  e = え        ko = こ        tsu = つ       no = の" +
+                        "\n  o = お        sa = さ          te = て       ha = は" +
+                        "\nka = か       shi = し          to = と         hi = ひ" +
+                        "\nsu = す        se = せ          na = な   　 fu = ふ" +
+                        "\nhe = へ       ho = ほ         ma = ま       mi = み");
     }
 }
